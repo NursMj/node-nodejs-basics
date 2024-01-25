db5ca04a8842787f7daea237ac9fbef9a6e7fe08
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-01-25 17:47:30 +0600

    implemented create fn

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+	const folderPath = __dirname + '/files';
+	const fileName = 'fresh.txt';
+	const filePath = path.join(folderPath, fileName);
+	const fileContent = 'I am fresh and young';
+
+	try {
+		if (fs.existsSync(filePath)) {
+			throw new Error('FS operation failed');
+		}
+		fs.writeFileSync(filePath, fileContent);
+		console.log(`File "${fileName}" created successfully at "${folderPath}"`);
+	} catch (error) {
+		console.error(`Error: ${error.message}`);
+	}
 };
 
 await create();
