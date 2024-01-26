be390d86504efc62cb3ae8824e7f66ba00f85f5e
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-01-26 14:54:11 +0600

    implimented copy fn

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+	const sourceFolder = __dirname + '/files';
+	const destinationFolder = __dirname + '/files_copys';
+
+	try {
+		if (fs.existsSync(destinationFolder) || !fs.existsSync(sourceFolder)) {
+			throw new Error('FS operation failed');
+		} else {
+			await fs.promises.mkdir(destinationFolder);
+
+			const files = await fs.promises.readdir(sourceFolder);
+
+			for (const file of files) {
+				const sourcePath = path.join(sourceFolder, file);
+				const destinationPath = path.join(destinationFolder, file);
+
+				await fs.promises.copyFile(sourcePath, destinationPath);
+			}
+		}
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 
 await copy();
