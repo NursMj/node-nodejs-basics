6783b5b8f24301cdb0970a9362dff14e457c1fae
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-01-26 16:09:34 +0600

    imlemented read fn

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
-const read = async () => {
-    // Write your code here 
+import fs, {promises} from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const list = async () => {
+    const folderPath = __dirname + '/files';
+	const fileName = 'fresh.txt';
+	const filePath = path.join(folderPath, fileName);
+
+	try {
+		if (!fs.existsSync(filePath)) {
+			throw new Error('FS operation failed');
+		} else {
+			const content = await promises.readFile(filePath, 'utf-8');
+
+            console.log('File content:\n', content);
+		}
+	} catch (error) {
+		console.log(error);
+	}
 };
 
-await read();
+await list();
