d2844990a214644747f4aa9cd850f2c8a31b3d06
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-01-26 15:32:34 +0600

    imlemented rename fn

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const revert = 0;
+
 const rename = async () => {
-    // Write your code here 
+	const wrongFilePath = __dirname + (!revert ? '/files/wrongFilename.txt' : '/files/properFilename.md');
+	const properFilePath = __dirname + (!revert ? '/files/properFilename.md' : '/files/wrongFilename.txt');
+
+	try {
+		if (fs.existsSync(properFilePath) || !fs.existsSync(wrongFilePath)) {
+			throw new Error('FS operation failed');
+		} else {
+			fs.rename(wrongFilePath, properFilePath, (error) => {
+				if (error) {
+					console.error(error);
+				} else {
+					console.log(`File renamed successfully: ${wrongFilePath} -> ${properFilePath}`);
+				}
+			});
+		}
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 await rename();
