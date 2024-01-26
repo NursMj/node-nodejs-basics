4d40e11c815608d44ffbcb20375d28873474f84b
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-01-26 16:01:59 +0600

    imlemented list fn

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const sourceFolder = __dirname + '/files';
+
+	try {
+		if (!fs.existsSync(sourceFolder)) {
+			throw new Error('FS operation failed');
+		} else {
+			const files = await fs.promises.readdir(sourceFolder);
+
+			for (const file of files) {
+				console.log(file);
+			}
+		}
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 await list();
