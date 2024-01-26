2865515cb480058878007510f2fe102f8c0bdcee
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-01-26 15:50:59 +0600

    imlemented delete fn

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = __dirname + '/files/fileToRemove.txt';
+
+    try {
+        if (fs.existsSync(filePath)) {
+            fs.unlinkSync(filePath);
+            console.log(`File deleted successfully: ${filePath}`);
+        } else {
+            throw new Error('FS operation failed');
+        }
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await remove();
