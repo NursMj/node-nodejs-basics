b40492cd8ba177c5828f2f8ca4cdd3a9b67643c3
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-01-26 17:12:05 +0600

    finished cli:args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+	const args = process.argv.slice(2);
+	const pairs = args
+		.filter((str) => str.startsWith('--'))
+		.map((prop, i) => `${prop} is ${args[args.indexOf(prop) + 1]}`)
+		.join(', ');
+
+	console.log(pairs);
 };
 
 parseArgs();
