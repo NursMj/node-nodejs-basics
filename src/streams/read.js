9a1bbba4700f20609d2bf018bc36b08e07cb8225
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-01-27 12:15:51 +0600

    imlemented streams: read fn

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const folderName = '/files';
+	const fileName = 'fileToRead.txt';
+	const filePath = path.join(__dirname, folderName, fileName);
+    const stream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+
+   
+    stream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    stream.on('error', (error) => {
+        console.error('Error reading file:', error);
+    });
+
 };
 
 await read();
