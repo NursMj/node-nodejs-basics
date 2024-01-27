81fddeb499599a653efb5e3b5910e189ded22635
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-01-27 13:59:21 +0600

    imlemented streams:write fn

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const write = async () => {
-    // Write your code here 
+    const folderName = '/files';
+	const fileName = 'fileToWrite.txt';
+	const filePath = path.join(__dirname, folderName, fileName);
+    const writableStream = fs.createWriteStream(filePath, { encoding: 'utf-8' });
+
+    process.stdin.pipe(writableStream);
+
+    writableStream.on('error', (error) => {
+        console.error('Error writing to file:', error);
+    });
 };
 
 await write();
