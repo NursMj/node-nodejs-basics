797eff3a33928fbff09b7612861b152d023aa2d1
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-01-27 11:30:17 +0600

    finished Hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const folderName = '/files';
+	const fileName = 'fileToCalculateHashFor.txt';
+	const filePath = path.join(__dirname, folderName, fileName);
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(filePath);
+
+    stream.on('data', (data) => {
+        hash.update(data);
+    });
+
+    stream.on('end', () => {
+        const fileHash = hash.digest('hex');
+        console.log(fileHash);
+    });
+
+    stream.on('error', (error) => {
+        console.error('Error reading file:', error);
+    });
+
 };
 
 await calculateHash();
