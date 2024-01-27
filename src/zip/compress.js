9f65100a236be6caeddb3c03f9c4843c958d2ce3
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2024-01-27 17:37:56 +0600

    implemented compress and decompress fns

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+	const folderPath = __dirname + '/files';
+	const fileName = 'fileToCompress.txt';
+	const compressedFileName = 'archive.gz';
+	const inputFilePath = path.join(folderPath, fileName);
+	const outputFilePath = path.join(folderPath, compressedFileName);
+	const readStream = fs.createReadStream(inputFilePath, { encoding: 'utf-8' });
+	const gzipStream = zlib.createGzip();
+	const writeStream = fs.createWriteStream(outputFilePath);
+
+	readStream.pipe(gzipStream).pipe(writeStream);
+
+	writeStream.on('finish', () => {
+		console.log(`File compressed to ${outputFilePath}`);
+	});
+
+	writeStream.on('error', (error) => {
+		console.error('Error compressing file:', error);
+	});
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const folderPath = __dirname + '/files';
+	const fileName = 'fileToCompress.txt';
+	const compressedFileName = 'archive.gz';
+	const inputFilePath = path.join(folderPath, compressedFileName);
+	const outputFilePath = path.join(folderPath, fileName);
+	const readStream = fs.createReadStream(inputFilePath);
+	const gunzipStream = zlib.createGunzip();
+	const writeStream = fs.createWriteStream(outputFilePath);
+
+	readStream.pipe(gunzipStream).pipe(writeStream);
+
+	writeStream.on('finish', () => {
+		console.log(`File decompressed to ${outputFilePath}`);
+	});
+
+	writeStream.on('error', (error) => {
+		console.error('Error compressing file:', error);
+	});
 };
 
 await decompress();
